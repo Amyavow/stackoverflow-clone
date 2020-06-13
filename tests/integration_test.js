cc4b11b79ac5b0317a9f41accf827391924ef70e
Amyavow <amy.avow@gmail.com>
2020-06-13 18:51:11 +0100

    finish test suite for auth

--- tests/integration_test.js
+++ tests/integration_test.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-// import User from '../src/models/User';
+import '@babel/polyfill';
+import User from '../src/models/User';
 // import Question from '../src/models/Question';
 import app from '../src/app';
 
@@ -9,11 +10,14 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 describe('Stackoverflow Clone API', () => {
-  describe('User concern test suite', () => {
+  describe('User auth concern test suite', () => {
+    after(async () => {
+      await User.deleteOne({ name: 'Testthisname' });
+    });
     it('should add user to the database', (next) => {
       const user = {
-        name: 'Testthis',
-        email: 'testthis@example.com',
+        name: 'Testthisname',
+        email: 'testthisemail@example.com',
         password: 'hashpassword',
         username: 'testthisusername',
       };
@@ -24,10 +28,27 @@ describe('Stackoverflow Clone API', () => {
         .end((err, res) => {
           expect(res).to.have.status(201);
           expect(res.body).to.be.an('object');
-          console.log(res.body);
           expect(res.body.success).to.be.equal(true);
           expect(res.body.data).to.be.an('object');
-          expect(res.body.data.user.email).to.be.equal('testthis@example.com');
+          expect(res.body.data.user.email).to.be.equal('testthisemail@example.com');
+          next();
+        });
+    });
+    it('should sign in an already registered user', (next) => {
+      const user = {
+        name: 'Testthisname',
+        email: 'testthisemail@example.com',
+        password: 'hashpassword',
+      };
+      chai.request(app)
+        .post('/api/v1/auth/signin')
+        .send(user)
+        .set('Content-Type', 'Application/json')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.sucess).to.be.equal(true);
+          expect(res.body).to.have.property('token');
           next();
         });
     });
